a9e7e47682b009e7a3356b36e58debc4ab0b74b8
rosebudgrave <1216680169@qq.com>
2021-12-06 14:53:07 +0800

    Initial commit

---
+++ src/main/java/com/hadoop/yarn/WordCountDriver.java
@@ -0,0 +1,26 @@
+package com.hadoop.yarn;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.util.Arrays;
+
+public class WordCountDriver {
+    private static Tool tool;
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+
+        switch (args[0]){
+            case "wordcount":
+                tool = new WordCount();
+                break;
+            default:
+                throw new RuntimeException("No such tool: "+args[0]);
+        }
+
+        int run = ToolRunner.run(conf,tool, Arrays.copyOfRange(args,1,args.length));
+
+        System.exit(run);
+    }
+}
